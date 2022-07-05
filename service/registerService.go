ab23db64adf075ba78ce767e3a0a7c81620fa0df
nil <n.spiller@gocaspi.de>
2022-07-05 11:14:46 +0200

    first commit

---
+++ service/registerService.go
@@ -0,0 +1,42 @@
+package service
+
+import (
+	"example-project/model"
+)
+
+//go:generate go run github.com/maxbrunsfeld/counterfeiter/v6 . DatabaseInterface
+type DatabaseInterface interface {
+	UpdateMany(docs []interface{}) (interface{}, error)
+	GetByID(id string) model.Employee
+}
+
+type EmployeeService struct {
+	DbService DatabaseInterface
+}
+
+func NewEmployeeService(dbInterface DatabaseInterface) EmployeeService {
+	return EmployeeService{
+		DbService: dbInterface,
+	}
+}
+
+func (s EmployeeService) CreateEmployees(employees []model.Employee) (interface{}, error) {
+
+	var emp []interface{}
+	for _, employee := range employees {
+		emp = append(emp, employee)
+
+	}
+
+	response, err := s.DbService.UpdateMany(emp)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
+
+func (s EmployeeService) GetEmployeeById(id string) model.Employee {
+	return s.DbService.GetByID(id)
+}
