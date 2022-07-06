f075fd19bad7939e7ad1af2b357e4a7af9aeea17
Luca Wolckenhauer <l.wolckenhauer@gocaspi.de>
2022-07-06 15:06:31 +0200

    added godotenv to project for environment variables

--- main.go
+++ main.go
@@ -5,11 +5,18 @@ import (
 	"example-project/middleware"
 	"example-project/model"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
 	databaseClient := datasource.NewDbClient(model.DbConfig{
-		URL:      "mongodb://localhost:27017",
+		URL:      os.Getenv("MONGO_URL"),
 		Database: "office",
 	})
 	engine := middleware.SetupEngine([]gin.HandlerFunc{middleware.SetupService(databaseClient)})
