a3324ec5f67005a9d1f96cad02b3f996355fec75
miled <m.souissi@gocaspi.de>
2022-07-08 13:58:33 +0200

    Creat DeleteEndponit

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	databaseClient := datasource.NewDbClient(model.DbConfig{
-		URL:      "mongodb://localhost:27017",
+		URL:      "mongodb://on4tdb:BMZQuk6pIL39nq46fOQPzygHtrhad5MFQMxs8YBQDW6YsJQSgbsIwO3aeOzlXXEnTjoz7ADVVr9jE1PKzU6GyQ==@on4tdb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@on4tdb@",
 		Database: "office",
 	})
 	engine := middleware.SetupEngine([]gin.HandlerFunc{middleware.SetupService(databaseClient)})
