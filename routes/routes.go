23e397feb1efa5da883123cdf85a5c3a86e4bd95
nil <n.spiller@gocaspi.de>
2022-07-12 21:05:01 +0200

    feature/ON4T-63_Pagination - tested OAuth with test-handlerFunctions in routes.go

--- routes/routes.go
+++ routes/routes.go
@@ -1,7 +1,9 @@
 package routes
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 //go:generate go run github.com/maxbrunsfeld/counterfeiter/v6 . HandlerInterface
@@ -16,11 +18,39 @@ type HandlerInterface interface {
 
 var Handler HandlerInterface
 
+const clientID = "69678bb4a1b8a0c2462f"
+const clientSecret = "cad250266a5613152d5a9ea64e70429545855782"
+
+type OAuthAccessResponse struct {
+	AccessToken string `json:"access_token"`
+}
+
 func CreateRoutes(group *gin.RouterGroup) {
 
 	group.POST("/Login", Handler.Login)
 	group.POST("/Logout", Handler.Logout)
 	group.POST("/register", Handler.CreateEmployeeHandler)
+	group.GET("/tester", func(context *gin.Context) {
+		context.JSON(200, "https://github.com/login/oauth/authorize?client_id=69678bb4a1b8a0c2462f")
+	})
+
+	group.GET("/authRedirect", func(context *gin.Context) {
+		code := context.Query("code")
+		reqURL := fmt.Sprintf("https://github.com/login/oauth/access_token?client_id=%s&client_secret=%s&code=%s", clientID, clientSecret, code)
+		req, err := http.NewRequest(http.MethodPost, reqURL, nil)
+		req.Header.Set("accept", "application/json")
+		if err != nil {
+			fmt.Println(err)
+		}
+		httpClient := http.Client{}
+		// Send out the HTTP request
+		res, _ := httpClient.Do(req)
+
+		fmt.Println(res)
+
+		//	code := context.Query("code")
+		context.JSON(200, code)
+	})
 
 	route := group.Group("/employee")
 	route.Use(Handler.ValidateToken)
