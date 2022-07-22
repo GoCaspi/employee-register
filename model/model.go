bad5c3fc7d493bb070c4c0ea22b1a70f9efb1198
nil <n.spiller@gocaspi.de>
2022-07-22 18:38:13 +0200

    feature/on4t-64_getEmployeesByDepartment - added Workload and shifts struct

--- model/model.go
+++ model/model.go
@@ -1,5 +1,9 @@
 package model
 
+import (
+	"time"
+)
+
 type Employee struct {
 	ID         string     `json:"id"`
 	FirstName  string     `json:"first_name"`
@@ -7,6 +11,7 @@ type Employee struct {
 	Email      string     `json:"email"`
 	Auth       HashedAuth `json:"auth" bson:"auth"`
 	Department string     `json:"department" bson:"department"`
+	Shifts     []Shift    `json:"shifts" bson:"shifts"`
 }
 type EmployeePayload struct {
 	ID         string `json:"id"`
@@ -46,4 +51,16 @@ type EmployeeReturn struct {
 type PaginatedPayload struct {
 	Employees []EmployeeReturn `json:"employees"`
 	PageLimit int              `json:"pageLimit"`
+}
+
+type Workload struct {
+	Duty  string    `json:"duty" bson:"duty"`
+	Start time.Time `json:"start" bson:"start"`
+	End   time.Time `json:"end" bson:"end"`
+	Total int       `json:"total" bson:"total"`
+}
+
+type Shift struct {
+	Week   int                 `json:"week" bson:"week"`
+	Duties map[string]Workload `json:"duties" bson:"duties"`
 }
