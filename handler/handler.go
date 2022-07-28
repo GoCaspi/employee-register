b0ba103634a925e02ef5c2737aabe123b410a71f
nil <n.spiller@gocaspi.de>
2022-07-28 12:03:59 +0200

    .env check

--- handler/handler.go
+++ handler/handler.go
@@ -484,3 +484,8 @@ func (handler Handler) ImplementrightManagementBoss(c *gin.Context) {
 	}
 
 }
+
+/*
+MONGO_URL=mongodb://on4tdb:BMZQuk6pIL39nq46fOQPzygHtrhad5MFQMxs8YBQDW6YsJQSgbsIwO3aeOzlXXEnTjoz7ADVVr9jE1PKzU6GyQ==@on4tdb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@on4tdb@
+MONGO_LOCAL=mongodb://localhost:27017
+*/
